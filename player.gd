9d9f6f617e48a4aa24138d2a2094726d5fabf165
alexandernair <alexnair@me.com>
2024-04-29 09:00:20 -0500

    worked on movement

--- player.gd
+++ player.gd
@@ -1,28 +1,48 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 200.0
+const ACCELERATION = 400
+const JUMP_VELOCITY = -300.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+@onready var animated_sprite_2d = $AnimatedSprite2D
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
+	apply_gravity(delta)
+	handel_jump()
+	var input_axis = Input.get_axis("ui_left", "ui_right")
+	handel_acceleration(input_axis, delta)
+	apply_friction(input_axis, delta)
+	update_animations(input_axis)
+	move_and_slide()
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+func apply_gravity(delta):
+		if not is_on_floor():
+			velocity.y += gravity * delta
+			
+func handel_jump():
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_up"):
+			velocity.y = JUMP_VELOCITY
 	else:
+		if Input.is_action_just_released("ui_up") and velocity.y < JUMP_VELOCITY/2:
+			velocity.y = JUMP_VELOCITY * 1/2
+			
+func apply_friction(input_axis, delta):
+	if(input_axis == 0):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	move_and_slide()
+func handel_acceleration(input_axis, delta):
+	if input_axis:
+		velocity.x = move_toward(velocity.x, SPEED * input_axis, ACCELERATION * delta)
+
+func update_animations(input_axis):
+	if input_axis != 0:
+		animated_sprite_2d.flip_h =(input_axis < 0)
+		animated_sprite_2d.play("run")
+	else:
+		animated_sprite_2d.play("idle")
+	if not is_on_floor():
+		animated_sprite_2d.play("jump")
