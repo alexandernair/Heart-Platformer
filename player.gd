48d57a4f1262f6989d9185b393f7374a560acc9f
Alexander Nair <alexnair@me.com>
2024-05-31 16:02:21 -0500

    fix wall jump

--- player.gd
+++ player.gd
@@ -7,10 +7,13 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var animated_sprite_2d = $AnimatedSprite2D
 @onready var coyote_jump_timer = $CoyoteJumpTimer
 @onready var starting_position = global_position
+@onready var wall_jump_timer = $WallJumpTimer
 
 var faster = false
 var air_jump = false 
 var wall_jumped = false
+var was_wall_normal = Vector2.ZERO
+
 func _physics_process(delta):
 	apply_gravity(delta)
 	handel_wall_jump()
@@ -22,18 +25,25 @@ func _physics_process(delta):
 	apply_air_resistance(input_axis, delta)
 	update_animations(input_axis)
 	var was_on_floor = is_on_floor()
+	var was_on_wall = is_on_wall_only()
+	if was_on_wall:
+		was_wall_normal = get_wall_normal();
 	move_and_slide()
 	var just_left_ledge = was_on_floor and not is_on_floor() and velocity.y >= 0
-	var wall_jumped = false
 	if just_left_ledge:
 		coyote_jump_timer.start()
+	wall_jumped = false
 	if Input.is_action_just_pressed("ui_accept") and !faster:
 		movement_data = load("res://FasterMovementData.tres")
 		faster = true
 	elif Input.is_action_just_pressed("ui_accept") and faster:
 		movement_data = load("res://DefaultDataMovement.tres")
 		faster = false
-
+	var just_left_wall = was_on_wall and not is_on_wall_only()
+	if just_left_wall:
+		wall_jump_timer.start()
+		
+		
 func apply_gravity(delta):
 		if not is_on_floor():
 			velocity.y += gravity * movement_data.gravity_scale * delta
@@ -46,14 +56,16 @@ func handel_jump():
 	elif not is_on_floor():
 		if Input.is_action_just_released("jump") and velocity.y < movement_data.jump_velocity/2:
 			velocity.y = movement_data.jump_velocity * 1/2
-		if Input.is_action_just_pressed("jump") and air_jump == true and not wall_jumped:
+		if Input.is_action_just_pressed("jump") and air_jump and not wall_jumped:
+			print("double jump time")
 			velocity.y = movement_data.jump_velocity * .8
 			air_jump = false
 			
 func handel_wall_jump():
-	if not is_on_wall_only():
-		return;
+	if not is_on_wall_only() and wall_jump_timer.time_left <= 0: return
 	var wall_normal = get_wall_normal();
+	if wall_jump_timer.time_left > 0.0:
+		wall_normal = was_wall_normal
 	if Input.is_action_just_pressed("jump") :
 		velocity.x = wall_normal.x * movement_data.speed
 		velocity.y = movement_data.jump_velocity
